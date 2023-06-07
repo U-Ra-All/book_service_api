6b81fadea1b64e08c0a49114c30df2b7bd350308
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-06-07 17:13:23 +0300

    Use BorrowingDetailSerializer in BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -2,9 +2,15 @@ from rest_framework import viewsets
 
 from books.permissions import IsAdminOrReadOnly
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
