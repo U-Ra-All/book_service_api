66f9faf3d29f97051df0786206d9fbc1b1b983e5
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-06-07 16:24:12 +0300

    Add dates validation in Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from books.models import Book
@@ -12,6 +13,26 @@ class Borrowing(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
     )
+
+    def clean(self):
+        if self.expected_return_date <= self.borrow_date:
+            raise ValidationError(
+                "expected_return_date should be later than borrow_date"
+            )
+        if self.actual_return_date <= self.borrow_date:
+            raise ValidationError("actual_return_date should be later than borrow_date")
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self):
         return f"User: {self.user}, book: {self.book}, expected return date: {self.expected_return_date}"
