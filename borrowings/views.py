be9fbfa97e68cb6ffb690fee11137ab73e21dbc5
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-06-08 08:54:08 +0300

    Implement CreateBorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,6 +1,12 @@
-from rest_framework import viewsets
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
 
-from books.permissions import IsAdminOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from books.models import Book
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
 
@@ -14,3 +20,35 @@ class BorrowingViewSet(viewsets.ModelViewSet):
             return BorrowingDetailSerializer
 
         return BorrowingSerializer
+
+
+class CreateBorrowingViewSet(APIView):
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        expected_return_date = serializer.data["expected_return_date"]
+        actual_return_date = serializer.data["actual_return_date"]
+        book = get_object_or_404(Book, pk=serializer.data["book"])
+
+        if book.inventory == 0:
+            raise ValidationError({"The book inventory equals 0!"})
+
+        user = self.request.user
+
+        book.inventory = book.inventory - 1
+        book.save()
+
+        Borrowing.objects.create(
+            expected_return_date=expected_return_date,
+            actual_return_date=actual_return_date,
+            book=book,
+            user=user,
+        )
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
