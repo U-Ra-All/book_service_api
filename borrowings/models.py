b5df8adacd98b4af351b936377807ed47ed224b6
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-06-08 09:59:44 +0300

    Add blank=True, null=True to actual_return_date field of Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -11,7 +11,7 @@ from books.models import Book
 class Borrowing(models.Model):
     borrow_date = models.DateTimeField(auto_now_add=True)
     expected_return_date = models.DateTimeField()
-    actual_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
