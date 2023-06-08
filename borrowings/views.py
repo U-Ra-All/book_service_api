e7896d6c89cf96d38c5fe8fa17e7d68713d7543a
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-06-08 16:13:02 +0300

    Implement ReturnBorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -2,13 +2,18 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from books.models import Book
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingDetailSerializer,
+    BorrowingReturnSerializer,
+)
+from users.models import User
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -46,12 +51,13 @@ class CreateBorrowingViewSet(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
+        borrow_date = serializer.data["borrow_date"]
         expected_return_date = serializer.data["expected_return_date"]
         actual_return_date = serializer.data["actual_return_date"]
         book = get_object_or_404(Book, pk=serializer.data["book"])
 
         if book.inventory == 0:
-            raise ValidationError({"The book inventory equals 0!"})
+            raise ValidationError("The book inventory equals 0!")
 
         user = self.request.user
 
@@ -59,6 +65,7 @@ class CreateBorrowingViewSet(APIView):
         book.save()
 
         Borrowing.objects.create(
+            borrow_date=borrow_date,
             expected_return_date=expected_return_date,
             actual_return_date=actual_return_date,
             book=book,
@@ -69,3 +76,27 @@ class CreateBorrowingViewSet(APIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class ReturnBorrowingViewSet(APIView):
+    serializer_class = BorrowingReturnSerializer
+    permission_classes = (IsAdminUser,)
+
+    def post(self, request, pk):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        borrowing = get_object_or_404(Borrowing, pk=pk)
+
+        if borrowing.actual_return_date:
+            raise ValidationError("The borrowing is already returned")
+
+        borrowing.actual_return_date = serializer.data.get("actual_return_date")
+        borrowing.save()
+
+        book = borrowing.book
+        book.inventory = book.inventory + 1
+        book.save()
+
+        return Response(status=status.HTTP_200_OK)
